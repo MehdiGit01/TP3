cf414949aaa1e319d83c99c723e5fa0a58b067b1
ZAOUI Mehdi <mehdi.zaoui@iutseine.u-paris.domain>
2024-02-07 11:02:33 +0100

    configuration des fichiers et affichage des topics

--- src/redpanda/admin.js
+++ src/redpanda/admin.js
@@ -6,7 +6,9 @@ const isLocalBroker = getLocalBroker()
 const redpanda = new Kafka({
     brokers: [
         isLocalBroker ? `${process.env.HOST_IP}:9092` : 'redpanda-0:9092',
-        'localhost:19092'],
+        '192.168.149.128:19092',
+    //    'localhost:19092'
+    ],
 })
 
 const admin = redpanda.admin()
--- src/redpanda/producer.js
+++ src/redpanda/producer.js
@@ -5,7 +5,9 @@ const isLocalBroker = getLocalBroker()
 const redpanda = new Kafka({
     brokers: [
         isLocalBroker ? `${process.env.HOST_IP}:9092` : 'redpanda-0:9092',
-        'localhost:19092'],
+        '192.168.149.128:19092',
+    //    'localhost:19092'
+    ],
 });
 const producer = redpanda.producer();
 export const getConnection = async (topic, user, message) => {
